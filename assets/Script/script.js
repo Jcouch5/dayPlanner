6728557819b0ac774dc8b3013f8b882221079e24
jcouch <jasoncouchjr410@gmail.com>
2022-03-31 22:54:20 -0400

    added functionality for saving to local storage and colored bgs

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -4,17 +4,42 @@ var hour = time.format("HH");
 var dateEl = document.querySelector("#currentDay");
 var saveBtn = document.querySelector('.saveBtn');
 var noteBlock = document.querySelectorAll('.time-block');
+var containerEl = $('.container');
+
 function setDate() {
     dateEl.textContent = date;
 }
 
 function saveNotes(e) {
     e.preventDefault();
-    notes = e.target.parentElement.children[1].textContent;
-    
+    notes = e.target.parentElement.children[1].value;
+    notesTime = e.target.parentElement.children[1].getAttribute('id');
+    console.log(notes);
+    localStorage.setItem(notesTime,notes);
+}
+
+function changeBg () {
+    for (i = 0; i < noteBlock.length; i++) {
+        var textareaEl = noteBlock[i].children[1];
+        var noteTime = textareaEl.getAttribute('id');
+        console.log(noteTime);
+        if (noteTime < hour) {
+          textareaEl.classList.add('past');
+          console.log(hour);
+         } else if (noteTime === hour) {
+          textareaEl.classList.add('present');
+        } else {
+            textareaEl.classList.add('future');
+         }
+    }
+}
+function bringPastText() {
+
+    for (i = 0; i < 18; i++) {
+        var noteBlockSet = noteBlock[i].children[1];
+        
+    }
 }
 setDate();
-console.log(noteBlock[0].children[1].textContent)
-
-
-saveBtn.addEventListener('click', saveNotes);
+changeBg();
+containerEl.on('click', '.saveBtn', saveNotes);
